19c53639a6388ee2313c3d1b599e3ac40c110e8e
channaveer <channaveer.b.swamy@gmail.com>
2022-04-29 16:54:00 +0530

    Made changes for the help

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.get("/about", (req, res) => {
 });
 
 app.get("/help", (req, res) => {
-    res.render("Help", {
+    res.render("help", {
         name: "channaveer",
         contact: "8888888888",
         title: "Help",
